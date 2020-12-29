bf234b353e981afd78627e2ea9da6898db00b96d
JongSeongYang <60866755+JongSeongYang@users.noreply.github.com>
2020-12-29 19:06:16 +0900

    리눅스 소켓 통신 기반 틱택토게임 서버/클라이언트

---
+++ server.c
@@ -0,0 +1,319 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+
+#define PORT 4250
+#define DATA_LEN 9
+
+int DATA_SEND_RECV(int);
+int Read(int);
+int Write(int);
+char check();
+void Print();
+void Put(char);
+void win();
+void lose();
+void draw();
+void end_init();
+
+bool myTurn = false;
+bool imFirst = true;
+int Round = 0;
+int game = 0;
+int score[2] = { 0, 0 };
+char board[9] = { 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e' };
+
+int main() {
+	int sock_flag, conn_flag, length;
+	struct sockaddr_in server_addr, client_addr;
+
+	if ((sock_flag = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	{
+		printf("Socket 생성 실패...\n");
+		exit(0);
+	}
+	else
+		printf("Socket 생성 성공...\n");
+
+	memset(&server_addr, 0x00, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(PORT);
+
+	if ((bind(sock_flag, (struct sockaddr*)&server_addr, sizeof(server_addr))) != 0)
+	{
+		perror("소켓 바인딩 실패...\n");
+		exit(0);
+	}
+	else
+		printf("소켓 바인딩 성공...\n");
+
+	if ((listen(sock_flag, 1)) != 0)
+	{
+		printf("연결 대기 실패...\n");
+		exit(0);
+	}
+
+	length = sizeof(client_addr);
+
+	if ((conn_flag = accept(sock_flag, (struct sockaddr*)&client_addr, &length)) < 0) {
+		printf("서버-클라이언트 연결 실패\n");
+		exit(0);
+	}
+	else
+		printf("서버-클라이언트 연결 성공\n");
+
+	DATA_SEND_RECV(conn_flag);
+	close(sock_flag);
+}
+
+int DATA_SEND_RECV(int socket_fd)
+{
+	int game_result;
+	char check_result;
+	char mychar;
+	char opchar;
+	bool endflag = false;
+	
+
+	while (1) {
+		// 종료메세지 수신하면 종료
+		if (endflag) {
+			break;
+		}
+		// 첫번째 라우드면
+		if (Round == 0) {
+			// 내가 선공이라면, O를 사용한다.
+			if (imFirst) {
+				mychar = 'o';
+				opchar = 'x';
+			}
+			else {
+				mychar = 'x';
+				opchar = 'o';
+			}
+
+			// 만약 0라운드이고, 내가 선공이라면 먼저 돌을 놓는다.
+			// 만약 0라운드이고, 내가 후공이라면 내 차례로 바꾸고 반복문 처음으로 간다
+			if (imFirst) {
+				Put(mychar);
+				Write(socket_fd);
+				myTurn = false;
+				Round++;
+				Print();
+				continue;
+			}
+			else {
+				myTurn = true;
+				Round++;
+				continue;
+			}
+		}
+		// 상대가 전송할때까지 대기한다.
+		while (1) {
+			sleep(1);
+			if (Read(socket_fd) > 0) {
+				myTurn = true;
+				break;
+			}
+		}
+
+		// 만약 내 차례가 됐다면, 
+		if (myTurn) {
+			//1. 종료조건을 검사한다.
+			check_result = check();
+			// 만약 항복을 했으면 점수 올리고 종료
+			if (check_result == 's') {
+				score[0] += 1;
+				win();
+				endflag = true;
+				end_init();
+				continue;
+			}
+			else if (check_result == mychar) { // 이긴 경우
+				score[0] += 1;
+				win();
+				end_init();
+				continue;
+			}
+			else if (check_result == opchar) { // 진 경우
+				score[1] += 1;
+				lose();
+				end_init();
+				continue;
+			}
+			else if (check_result == 'd') { // 비긴 경우
+				draw();
+				end_init();
+				continue;
+			}
+
+			// 2. 화면에 게임판 띄우기 (TODO)
+			Print();
+
+			// 3. 놓을 곳을 클릭으로 입력 받기
+			Put(mychar);
+
+			// 4. 종료조건 검사
+			check_result = check();
+			if (check_result == 's') {// 내가 항복한 경우 
+				score[1] += 1;
+				Write(socket_fd);
+				lose();
+				endflag = true;
+				end_init();
+				continue;
+			}
+			else if (check_result == mychar) { // 이긴 경우
+				score[0] += 1;
+				Write(socket_fd);
+				win();
+				end_init();
+				continue;
+			}
+			else if (check_result == opchar) { // 진 경우
+				score[1] += 1;
+				Write(socket_fd);
+				lose();
+				end_init();
+				continue;
+			}
+			else if (check_result == 'd') { // 비긴 경우
+				Write(socket_fd);
+				draw();
+				end_init();
+				continue;
+			}
+
+			// 5. 화면에 게임판 띄우기 (TODO)
+			Print();
+
+			// 6. 게임판 전송
+			Write(socket_fd);
+			myTurn = false;
+			Round++;
+		}
+	}
+	printf("[%dGame] score -> my(%d) :op(%d)\n", game, score[0], score[1]);
+	return 0;
+}
+void end_init() {
+	imFirst = !imFirst;
+	Round = 0;
+	game++;
+	memset(board, 'e', DATA_LEN);
+}
+int Read(int socket_fd) {
+	char data[DATA_LEN];
+	memset(board, 0x00, DATA_LEN);
+	int readbyte = read(socket_fd, data, DATA_LEN);
+	if (readbyte < 0) {
+		printf("READ socket error!\n");
+		return readbyte;
+	}
+	else {
+		printf("READ socket success!\n");
+		for (int i = 0; i < DATA_LEN; i++) {
+			board[i] = data[i];
+		}
+		return readbyte;
+	}
+}
+int Write(int socket_fd) {
+	int writebyte = write(socket_fd, board, DATA_LEN);
+	if (writebyte < 0) {
+		printf("WRITE socket error!\n");
+		return writebyte;
+	}
+	else {
+		printf("WRITE socket success!\n");
+		return writebyte;
+	}
+}
+void win() {
+	printf("win\n");
+}
+void lose() {
+	printf("lose\n");
+}
+void draw() {
+	printf("draw\n");
+}
+void Print() {
+	printf("%c %c %c\n", board[0], board[1], board[2]);
+	printf("%c %c %c\n", board[3], board[4], board[5]);
+	printf("%c %c %c\n", board[6], board[7], board[8]);
+}
+void Put(char myChar) {
+	int put = 0;
+	while (1) {
+		// 놓을 곳 입력
+		printf("put : ");
+		scanf("%d", &put);
+		getchar();
+		if (put == 9) {
+			memset(board, 0x00, DATA_LEN);
+			strcpy(board, "endgame");
+			break;
+		}
+		else if (board[put] == 'e') {
+			board[put] = myChar;
+			break;
+		}
+		else
+		{
+			printf("can't put %d\n", put);
+			continue;
+		}
+	}
+}
+char check() {
+	int cnt = 0;
+	if (strncmp(board, "endgame", 7) == 0) {
+		return 's';
+	}
+	else {
+		for (int i = 0; i < DATA_LEN; i++) {
+			if (board[i] == 'e')
+				cnt++;
+		}
+		if ((board[0] == board[1]) && (board[1] == board[2]) && (board[0] != 'e')) {
+			return board[0];
+		}
+		else if ((board[3] == board[4]) && (board[4] == board[5]) && (board[3] != 'e')) {
+			return board[3];
+		}
+		else if ((board[6] == board[7]) && (board[7] == board[8]) && (board[6] != 'e')) {
+			return board[6];
+		}
+		else if ((board[0] == board[3]) && (board[3] == board[6]) && (board[0] != 'e')) {
+			return board[0];
+		}
+		else if ((board[1] == board[4]) && (board[4] == board[7]) && (board[1] != 'e')) {
+			return board[1];
+		}
+		else if ((board[2] == board[5]) && (board[5] == board[8]) && (board[2] != 'e')) {
+			return board[2];
+		}
+		else if ((board[0] == board[4]) && (board[4] == board[8]) && (board[0] != 'e')) {
+			return board[0];
+		}
+		else if ((board[2] == board[4]) && (board[4] == board[6]) && (board[2] != 'e')) {
+			return board[2];
+		}
+		else if (cnt == 0) {
+			return 'd';
+		}
+		else {
+			return 'e';
+		}
+	}
+}
